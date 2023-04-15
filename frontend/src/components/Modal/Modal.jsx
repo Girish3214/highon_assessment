ee1accd039e38c07df346efd161c08e5d6e63665
Girish <m.girishroy@gmail.com>
2023-04-15 15:36:41 +0530

    logout button functionality changes and user data to localStorage

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -4,6 +4,14 @@ import ReactDom from "react-dom";
 import "./modal.css";
 
 function Modal({ open, onClose, logout }) {
+  const logoutHandler = () => {
+    logout({
+      logoutParams: {
+        returnTo: window.location.origin + "/login",
+      },
+    });
+    localStorage.removeItem("chat-user");
+  };
   if (!open) return null;
   return ReactDom.createPortal(
     <>
@@ -13,17 +21,7 @@ function Modal({ open, onClose, logout }) {
           <div className="modal-details-container">
             <p>Do you want to LOGOUT</p>
             <div className="modal-buttons-container">
-              <button
-                onClick={() =>
-                  logout({
-                    logoutParams: {
-                      returnTo: window.location.origin + "/login",
-                    },
-                  })
-                }
-              >
-                Yes
-              </button>
+              <button onClick={() => logoutHandler()}>Yes</button>
               <button onClick={() => onClose()}>No</button>
             </div>
           </div>
--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -8,18 +8,14 @@ import Exit from "../../assets/icons/exit.png";
 import ProfileCard from "../../components/ProfileCard";
 import Modal from "../../components/Modal/Modal";
 import axios from "../../utils/axios";
+import PageSpinner from "../../components/page-spinner/PageSpinner";
 
 const HomePage = () => {
   const { isAuthenticated, logout, getIdTokenClaims, isLoading } = useAuth0();
   const navigate = useNavigate();
-  console.log(isAuthenticated, isLoading);
-  const getToken = async () => {
-    const token = await getIdTokenClaims();
-    console.log(token);
-    return token;
-  };
 
   const [logoutOpen, setLogoutOpen] = useState(false);
+  const [userData, setUserData] = useState(undefined);
   const [profiles, setProfiles] = useState(["1", "2", "3"]);
 
   const [unchattedProfiles, setunchattedProfiles] = useState([
@@ -27,25 +23,51 @@ const HomePage = () => {
     { id: "2", name: "Card 2", email: "card2@gmail.com" },
   ]);
 
-  const onClose = (name) => {
+  const getToken = async () => {
+    const token = await getIdTokenClaims();
+    return token;
+  };
+  const onClose = () => {
     setLogoutOpen(false);
   };
 
+  const getAllUsers = async (id) => {
+    const { data } = await axios.get(`/users/all/${id}`);
+    setProfiles(data);
+  };
+
   useEffect(() => {
-    if (!isLoading && !isAuthenticated) {
+    if (userData?._id || localStorage.getItem("chat-user")?._id) {
+      getAllUsers(userData?._id || localStorage.getItem("chat-user")._id);
+    }
+    return () => {};
+  }, [userData, localStorage.getItem("chat-user")]);
+
+  useEffect(() => {
+    if (!isLoading && !isAuthenticated && !localStorage.getItem("chat-user")) {
       navigate("/login");
     }
-
-    if (!isLoading && isAuthenticated) {
+    if (!isLoading && isAuthenticated && !localStorage.getItem("chat-user")) {
       getToken().then(async (res) => {
-        console.log(res);
-        await axios.post("/users", {
+        const { data } = await axios.post("/users", {
           username: res.name,
-          email: res.email,
+          email: res?.email ?? res.name + "@gmail.com",
           avatarImage: res.picture,
         });
+
+        setUserData(data);
+        localStorage.setItem(
+          "chat-user",
+          JSON.stringify({ ...res, _id: data._id })
+        );
       });
-      // axios.post()
+    } else if (
+      !isLoading &&
+      isAuthenticated &&
+      localStorage.getItem("chat-user")
+    ) {
+      const localUser = JSON.parse(localStorage.getItem("chat-user"));
+      getAllUsers(localUser._id);
     }
 
     return () => {};
@@ -53,40 +75,50 @@ const HomePage = () => {
 
   return (
     <>
-      <Modal open={logoutOpen} onClose={onClose} logout={logout} />
-      <div className="home-main-container">
-        <div className="home-container-header">
-          <div className="app-title">A-Chat</div>
-          <button onClick={() => setLogoutOpen(true)}>
-            <img src={Exit} alt="exit" />{" "}
-          </button>
-        </div>
-        <section className="home-container-profiles">
-          <div
-            className="profiles-container"
-            style={{ justifyContent: profiles.length < 4 && "center" }}
-          >
-            {profiles.map((item, index) => (
-              <div className="profile" key={item + index}>
-                {item}
-                <div className="profile-notification" />
+      {isLoading ? (
+        <PageSpinner />
+      ) : (
+        <>
+          <Modal open={logoutOpen} onClose={onClose} logout={logout} />
+          <div className="home-main-container">
+            <div className="home-container-header">
+              <div className="app-title">iChat</div>
+              <button onClick={() => setLogoutOpen(true)}>
+                <img src={Exit} alt="exit" />{" "}
+              </button>
+            </div>
+            <section className="home-container-profiles">
+              <div
+                className="profiles-container"
+                style={{ justifyContent: profiles.length < 4 && "center" }}
+              >
+                {profiles.map((item, index) => (
+                  <div
+                    className="profile"
+                    key={item}
+                    onClick={() => navigate(`/chat/${item._id}`)}
+                  >
+                    <img src={item.avatarImage} />
+                    <div className="profile-notification" />
+                  </div>
+                ))}
               </div>
-            ))}
+            </section>
+            <div className="unchatted-main-profiles-container">
+              <div className="unchatted-profiles-container">
+                {unchattedProfiles.map((card, index) => (
+                  <ProfileCard
+                    key={index}
+                    name={card.name}
+                    email={card.email}
+                    id={card.id}
+                  />
+                ))}
+              </div>
+            </div>
           </div>
-        </section>
-        <div className="unchatted-main-profiles-container">
-          <div className="unchatted-profiles-container">
-            {unchattedProfiles.map((card, index) => (
-              <ProfileCard
-                key={index}
-                name={card.name}
-                email={card.email}
-                id={card.id}
-              />
-            ))}
-          </div>
-        </div>
-      </div>
+        </>
+      )}
     </>
   );
 };
