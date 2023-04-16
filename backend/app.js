fa128ca43630c3e026a189a5decf33f60fab99eb
Girish <m.girishroy@gmail.com>
2023-04-16 19:53:29 +0530

    unwant opackages removal

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import morgan from "morgan";
 import cors from "cors";
 import "express-async-errors";
 import { Server } from "socket.io";
